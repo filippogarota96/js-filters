01f6935ebebb4f23a8bb35eef76167c2ceed88ce
Filippo Garota <filogarota101@gmail.com>
2021-07-20 16:09:21 +0200

    milestone 3
    modifica dinamica dei filtri

--- js/script.js
+++ js/script.js
@@ -18,16 +18,58 @@ window.addEventListener('load', function () {
       
       let image = document.querySelector('#myImg');
       image.src = URL.createObjectURL(this.files[0]); 
+
+      //hide show
       const upload = document.getElementById('upload');
       const imgCnt = document.getElementById('cnt');
       const filters = document.getElementById('filters');
       upload.style.display = "none";
       imgCnt.style.display = "block";
       filters.style.display = "block";
+      const img = document.getElementById("myImg");
+      const greyscale = document.getElementById("greyscale");
+      const saturate = document.getElementById("saturate");
+      const sepia = document.getElementById("sepia");
+      const invert = document.getElementById("invert");
+      const contrast = document.getElementById("contrast");
+      const brightness = document.getElementById("brightness");
+      const blur = document.getElementById("blur");
+      const hue = document.getElementById("hue");
 
-      var greyscale = document.getElementById('greyscale').value;
-      console.log(greyscale);
 
+      
+      greyscale.addEventListener('change', function(){
+        img.style.filter = "grayscale(" + greyscale.value + "%)";
+      });
+
+      saturate.addEventListener('change', function () {
+        img.style.filter = "saturate(" + saturate.value + "%)";
+      });
+
+      sepia.addEventListener('change', function () {
+        img.style.filter = "sepia(" + sepia.value + "%)";
+      });
+
+      invert.addEventListener('change', function () {
+        img.style.filter = "invert(" + invert.value + "%)";
+      });
+
+      contrast.addEventListener('change', function () {
+        img.style.filter = "contrast(" + contrast.value + "%)";
+      });
+
+      brightness.addEventListener('change', function () {
+        img.style.filter = "brightness(" + brightness.value + "%)";
+      });
+
+      blur.addEventListener('change', function () {
+        img.style.filter = "blur(" + blur.value + "px)";
+      });
+
+      hue.addEventListener('change', function () {
+        img.style.filter = "hue-rotate(" + hue.value + "deg)";
+      });
+      
     }
   });
 });
